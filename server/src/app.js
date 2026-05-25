936d65ad5e526a6eb9dad6b2ad2f3b408b36d69d
Chandureddy0405 <yannamchandrashekharreddy@gmail.com>
2026-05-25 23:03:06 +0530

    Implement JWT authentication system

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,29 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(morgan("dev"));
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Backend Auto Restart Working");
+  res.send("JobPilot AI Backend Running");
 });
 
 app.listen(PORT, () => {
